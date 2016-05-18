3cb2360517ef83f102ce6881123d7632a8de7edf
evohnave <eric.vanhove@gmail.com>
2016-05-18 15:49:18 -0400

    Added in check that there are items to be downloaded. Made into a function

--- main.r
+++ main.r
@@ -1,11 +1,15 @@
 # Main script
-source("Seljuk.r")
-SeljuksDF <- InitializeSeljuks()
-CompletedItems <- GetCompletedItems()
-CompletedItems <- RemoveDoneItems(CompletedItems, SeljuksDF$Item_Number)
-for(i in 1:(length(CompletedItems))){
-    SeljuksDF <- Get1CompletedItem(CompletedItems[i], SeljuksDF)
+main <- function(){
+    source("Seljuk.r")
+    SeljuksDF <- InitializeSeljuks()
+    CompletedItems <- GetCompletedItems()
+    CompletedItems <- RemoveDoneItems(CompletedItems, SeljuksDF$Item_Number)
+    if(length(CompletedItems)>0){
+        for(i in 1:(length(CompletedItems))){
+            SeljuksDF <- Get1CompletedItem(CompletedItems[i], SeljuksDF)
+        }
+    }
+    CloseSeljuks(SeljuksDF)
+    # Clean up
+    rm(list = ls())
 }
-CloseSeljuks(SeljuksDF)
-# Clean up
-rm(list = ls())
