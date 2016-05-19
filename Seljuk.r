ef5ec584d0c71e90257a15d2ead333b394850221
evohnave <eric.vanhove@gmail.com>
2016-05-19 08:02:02 -0400

    IdentifyFileLibrary: Updated SP3 setting

--- Seljuk.r
+++ Seljuk.r
@@ -103,7 +103,7 @@ IdentifyFileLibrary <- function(){
     df <- matrix(data = c(
         "SEIP06-W7", "DS", "DT", "DT2", "SP3",
         "C:/Users/eav1/Dropbox/Seljuk/", "//DS/Seljuk/", "C:/Users/Cire/Dropbox/Seljuk",
-        "//DT2/Seljuk/", "//SP3/Seljuk/"), ncol = 2,byrow = FALSE)
+        "//DT2/Seljuk/", "C:/Users/Eric/Dropbox/Seljuk"), ncol = 2,byrow = FALSE)
     return(df[df[,1]==Sys.info()["nodename"],2])
 }
 
