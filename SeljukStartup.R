174d8729f022704abc440042b9ad154c4dd7d52c
evohnave <eric.vanhove@gmail.com>
2016-09-27 08:29:29 -0400

    SeljukStartup: fixed typo on setwd. New coins to Solidus.csv

--- SeljukStartup.R
+++ SeljukStartup.R
@@ -1,6 +1,6 @@
 SeljukStartUp <- function(){
     wd <- getwd()
-    if(!grepl("/Dropbox/Seljuk", wd)) setw("C:/Users/eav1/Dropbox/Seljuk")
+    if(!grepl("/Dropbox/Seljuk", wd)) setwd("C:/Users/eav1/Dropbox/Seljuk")
     listOf <- dir(pattern = "*.r$")
     res <- lapply(X = listOf,FUN = source)
 }
